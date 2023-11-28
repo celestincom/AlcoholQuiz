9fb7f7f3048df349fae3381afb2baccb1cda68f5
celestincom <coman_celestin@yahoo.com>
2023-11-28 15:34:01 +0200

    Before setting answer typos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import QuizView from "./components/QuizView";
+import ScoreView from "./components/ScoreView";
 
 function App() {
+  const questions = [
+    {
+      question: "Care este activitatea ta preferată într-o seară relaxantă?",
+      answers: [
+        { text: "Citit o carte"},
+        { text: "Vizionat un film sau serial", isCorrect: true },
+        { text: "Ascultat muzică" },
+        { text: "Plimbare în natură" },
+      ],
+    },
+    {
+      question: "Ce fel de vacanță preferi?",
+      answers: [
+        { text: "O excursie într-un oraș istoric" },
+        { text: "Relaxare pe o plajă tropicală" },
+        { text: "Aventură în munți" },
+        { text: "Călătorie culturală prin Europa", isCorrect: true },
+      ],
+    },
+    {
+      question: "Care este mâncarea ta preferată?",
+      answers: [
+        { text: "Pizza" },
+        { text: "Sushi", isCorrect: true },
+        { text: "Biftek la grătar" },
+        { text: "Paste" },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [isQuizOver, setIsQuizOver] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerClick = (isCorrect) => {
+    // check score
+    if (isCorrect) setScore(score + 1);
+
+    const next = currentQuestion + 1;
+    if (next < questions.length) setCurrentQuestion(next);
+    else setIsQuizOver(true);
+  };
+
+  const handleResetClick = () => {
+    // fix: score not resetting
+    setScore(0);
+
+    setCurrentQuestion(0);
+    setIsQuizOver(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Screen">
+      <div className="App">  
+        {/* {isQuizOver ? (
+          <ScoreView handleResetClick={handleResetClick} score={score} />
+        ) : (         */}
+          <QuizView
+            questions={questions}
+            currentQuestion={currentQuestion}
+            handleAnswerClick={handleAnswerClick}
+          />
+        {/* )} */}
+      </div>
     </div>
   );
 }
